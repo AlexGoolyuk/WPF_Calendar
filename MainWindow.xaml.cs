abffc6922bb0b4ec1f42d7c62d2d6b434fe23721
AlexGoolyuk <61875038+AlexGoolyuk@users.noreply.github.com>
2021-03-05 15:09:31 +0200

    git commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿//Program for testing calendar props for work with days
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace CalendarTesting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalendarDayButton_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            MessageBox.Show("Test");
+        }
+    }
+}
